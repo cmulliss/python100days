cb18a1df2f3bfbe4113aeda7ecfd37d14e32c22d
cmulliss <cmulliss@gmail.com>
2021-10-17 17:48:20 +0100

    conditionals

---
+++ daysLife.py
@@ -0,0 +1,11 @@
+age = input("What is your age in years? ")
+
+years = 90 - int(age)
+
+days = years * 365
+
+weeks = years * 52
+
+months = years * 12
+
+print(f"You have {days} days, or {weeks} weeks or {months} months left. ")
