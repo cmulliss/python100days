1c0fb5f338a7e7f617642414e840c561315cec16
cmulliss <cmulliss@gmail.com>
2021-10-21 17:28:29 +0100

    lists

---
+++ coin_toss.py
@@ -0,0 +1,7 @@
+import random
+
+random_face = random.randint(0, 1)
+if random_face == 1:
+    print("Heads")
+else:
+    print("Tails")
---
+++ random_numbers.py
@@ -0,0 +1,9 @@
+import random
+
+# gives an int between 1 and 10
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+# gives a float between 0 and 1
+random_float = random.random()
+print(random_float)
