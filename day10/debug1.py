1183eb222f2aeecb978bb93d456b319361ca3c5e
cmulliss <cmulliss@gmail.com>
2021-11-27 17:06:01 +0000

    more debugging

--- day10/debug1.py
+++ day10/debug1.py
@@ -20,31 +20,38 @@
 # print(dice_imgs[dice_num])
 
 # # Play Computer
-year = int(input("What's your year of birth?"))
-if year > 1980 and year < 1994:
-  print("You are a millenial.")
-elif year > 1994:
-  print("You are a Gen Z.")
+# year = int(input("What's your year of birth? "))
+# if year > 1946 and year < 1964:
+#     print("You are a baby boomer")
+# elif year > 1980 and year < 1994:
+#     print("You are a millenial.")
+# elif year > 1994:
+#     print("You are a Gen Z.")
+# else:
+#     print("You don't fit into any of these silly categories!")
 
 # # Fix the Errors
-# age = input("How old are you?")
+# age = int(input("How old are you? "))
 # if age > 18:
-# print("You can drive at age {age}.")
+#     print(f"You can drive at age {age}.")
+# else:
+#     print(f"You are too young to drive at {age}")
 
-# #Print is Your Friend
+# Print is Your Friend
 # pages = 0
 # word_per_page = 0
 # pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: "))
+# word_per_page = int(input("Number of words per page: "))
 # total_words = pages * word_per_page
 # print(total_words)
 
 # #Use a Debugger
-# def mutate(a_list):
-#   b_list = []
-#   for item in a_list:
-#     new_item = item * 2
-#   b_list.append(new_item)
-#   print(b_list)
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        b_list.append(new_item)
+    print(b_list)
 
-# mutate([1,2,3,5,8,13])
+
+mutate([1, 2, 3, 5, 8, 13])
---
+++ day10/lists.py
@@ -0,0 +1,8 @@
+# my_list = ["hello"]
+# print(my_list)
+# print(my_list[])
+
+mylist = [0, 1, 2, 3, 4, 5]
+print(mylist[0:6])
+# gives: [0, 1, 2, 3, 4, 5]
+print(mylist[0:-1])
